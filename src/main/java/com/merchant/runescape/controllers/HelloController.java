374fa57835034aa08430c37968646e6c698ac796
tulmin <soulyyy@gmail.com>
2017-04-19 18:07:00 +0300

    init

---
+++ src/main/java/com/merchant/runescape/controllers/HelloController.java
@@ -0,0 +1,11 @@
+package com.merchant.runescape.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class HelloController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "Hola, merchant";
+  }
+}
